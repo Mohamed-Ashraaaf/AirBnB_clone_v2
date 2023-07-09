e69b4c6f5b66b608a86f0e240cd7f005ac641855
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-07-09 11:54:40 +0000

    more fixes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,19 +2,20 @@
 """ Fabfile that create a .tgz archive from
 the contents of web_static folder"""
 
+from fabric.api import run, local
 from datetime import datetime
-from fabric.api import local
-from os.path import isdir
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """Generates a .tgz archive"""
+
+    format_d = "%Y%m%d%H%M%S"
+    dir_path = "versions/web_static_"
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        local("mkdir -p versions")
+        date = datetime.now()
+        filename = dir_path + date.strftime(format_d) + ".tgz"
+        local("tar -cvzf " + filename + " web_static")
+        return filename
     except:
         return None
